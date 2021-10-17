0d4be89d581b4ca6ecb5231ad2fa344a84c53345
Grimaldi Baptiste <79135575+GrimalDev@users.noreply.github.com>
2021-10-17 16:44:32 +0200

    commited

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const discord = require('discord')
+require('dotenv').config();
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const { TOKEN } = process.env;
+
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("messageCreate", msg => {
+    if(msg.content === "ping") {
+        msg.channel.send("pong")
+    }
+})
+
+client.login(process.env.TOKEN)
